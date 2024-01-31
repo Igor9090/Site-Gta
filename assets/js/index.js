925a6d686db68d7d6cc4f482a75fba3732e0e0d4
IgorVn <igorvinicin123@gmail.com>
2024-01-31 20:03:20 -0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataforma = document.querySelector(
+  ".btn-plataforma .plataformas"
+);
+
+botao.addEventListener("click", () => {
+ elementoPlataforma.classList.toggle("ativo")
+});
